bb8380e54a11f931d0d7fed07b026940e0eacf47
AndreyBychkov <abychkov@edu.hse.ru>
2019-12-16 00:12:37 +0300

    Specified behavior of is_polynomial function and expanded according tests

--- PythonPackage/AST_walk.py
+++ PythonPackage/AST_walk.py
@@ -4,8 +4,16 @@ import sympy as sp
 def is_polynomial_function(expr: sp.Expr):
     return True if expr.is_Add or \
                    expr.is_Mul or \
-                   (expr.is_Pow and expr.args[1].is_Number) \
+                   _is_positive_numeric_power(expr) \
         else False
+
+
+def _is_positive_numeric_power(expr: sp.Expr):
+    if not expr.is_Pow:
+        return False
+
+    power = expr.args[1]
+    return power.is_Number and power > 0
 
 
 def find_non_polynomial(expr: sp.Expr):
--- PythonPackage/tests.py
+++ PythonPackage/tests.py
@@ -16,10 +16,26 @@ def test_symbols_holder():
     assert not symbols_names.symmetric_difference(expected_names)
 
 
-def test_AST_walk():
+def test_AST_walk_polynomial_expr():
+    x, y = sp.symbols(["x", "y"])
+
+    expr = x ** 2 + y - x * y + y ** 3
+    found = find_non_polynomial(expr)
+    assert found is None
+
+
+def test_AST_walk_rational_expr():
+    x, y = sp.symbols(["x", "y"])
+
+    expr = (x ** 2 + y) / (x + y ** 2)
+    found = find_non_polynomial(expr)
+    assert found == 1 / (x + y ** 2)
+
+
+def test_AST_walk_general_expr():
     x, y = sp.symbols(["x", "y"])
     sin = sp.Function("sin")
-    expr = x ** 2 - y + (x + y) * sin(x)
 
+    expr = x ** 2 - y + (x + y) * sin(x)
     found = find_non_polynomial(expr)
     assert found == sin(x)
