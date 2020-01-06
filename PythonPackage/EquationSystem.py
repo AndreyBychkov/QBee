02bd7253dfae5558c830604e85185aeeac49b9ad
AndreyBychkov <abychkov@edu.hse.ru>
2020-01-06 19:51:50 +0300

    Merged parts of equation system into one

--- PythonPackage/EquationSystem.py
+++ PythonPackage/EquationSystem.py
@@ -8,37 +8,34 @@ from SymbolsHolder import SymbolsHolder
 
 class EquationSystem:
     def __init__(self, equations: List[sp.Eq]):
-        self.original_equations = equations
-        self.added_equations = list()
-        self.replacement_equations = list()
+        self._equations = equations
+        self._replacement_equations = list()
 
         self.variables = SymbolsHolder(reduce(set.union, map(lambda e: e.free_symbols, equations)))
 
-    def get_equations(self):
-        return self.original_equations + self.added_equations
+    @property
+    def equations(self):
+        return self._equations
 
     def replace_expression(self, old: sp.Expr, new: sp.Expr):
-        for i in range(len(self.original_equations)):
-            self.original_equations[i] = self.original_equations[i].subs(old, new)
+        for i in range(len(self._equations)):
+            self._equations[i] = self._equations[i].subs(old, new)
 
     def expand_equations(self):
-        for i in range(len(self.original_equations)):
-            self.original_equations[i] = sp.expand(self.original_equations[i])
-
-        for i in range(len(self.added_equations)):
-            self.added_equations[i] = sp.expand(self.added_equations[i])
+        for i in range(len(self._equations)):
+            self._equations[i] = sp.expand(self._equations[i])
 
     def is_polynomial(self):
         non_polynomial_found = None
-        for eq in self.original_equations + self.added_equations:
+        for eq in self._equations:
             non_polynomial_found = find_non_polynomial(eq)
         return False if non_polynomial_found else True
 
     def __len__(self):
-        return self.original_equations.__len__() + self.added_equations.__len__()
+        return len(self._equations)
 
     def __repr__(self):
-        return '\n'.join(map(lambda e: e.__repr__(), self.get_equations()))
+        return '\n'.join(map(lambda e: e.__repr__(), self._equations))
 
     def __str__(self):
-        return '\n'.join(map(lambda e: e.__str__(), self.get_equations()))
+        return '\n'.join(map(lambda e: e.__str__(), self._equations))
