2f93c06593b0ad114778a6e8ee9038cb40d8a15e
AndreyBychkov <abychkov@edu.hse.ru>
2020-01-23 03:06:26 +0300

    Added monomial decompositions with examples

---
+++ PythonPackage/combinations.py
@@ -0,0 +1,40 @@
+import itertools
+import sympy as sp
+
+from functools import reduce, partial
+from operator import mul
+
+
+def get_decompositions(monomial: sp.Poly):
+    res = _get_decompositions_rec(monomial, [], set())
+    res = map(lambda s: sorted(s, key=sp.polys.orderings.monomial_key('grlex', monomial.free_symbols)), res)
+    res = map(lambda b: list(map(lambda m: m.as_expr(), b)), res)  # For convenient representation. Can be removed if necessary.
+    res = set(tuple(s) for s in res)
+    return res
+
+
+def _get_decompositions_rec(monomial: sp.Poly, decomposition: list, result: set):
+    if monomial.total_degree() <= 2:
+        return result.add(tuple([monomial] + decomposition))
+
+    for divisor in _get_divisors(monomial):
+        _get_decompositions_rec(sp.div(monomial, divisor)[0], decomposition + [divisor], result)
+    return result
+
+
+def _get_divisors(expr: sp.Poly):
+    return _get_possible_squares(expr) + _get_mul_combinations(expr)
+
+
+def _get_mul_combinations(expr: sp.Poly):
+    res = itertools.combinations(expr.free_symbols, 2)
+    res = map(partial(reduce, mul), res)
+    res = map(sp.poly, res)
+    return list(res)
+
+
+def _get_possible_squares(expr: sp.Poly):
+    res = filter(lambda var: expr.degree(var) > 1, expr.free_symbols)
+    res = map(lambda var: var ** 2, res)
+    res = map(sp.poly, res)
+    return list(res)
