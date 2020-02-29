24a08d42129764083c9ac4b673411154430e5980
AndreyBychkov <abychkov@edu.hse.ru>
2020-03-01 01:44:40 +0300

    Added quadratic_linearization to main file

--- PythonPackage/qbee.py
+++ PythonPackage/qbee.py
@@ -26,3 +26,45 @@ def polynomialize(system: List[sp.Eq],
     eq_system = EquationSystem(system, parameter_variables, input_variables)
     eq_system.polynomialize(mode)
     return eq_system.equations
+
+
+def quadratic_linearize(system: List[sp.Eq],
+                        parameter_variables: Iterable[sp.Symbol] = None,
+                        input_variables: Iterable[sp.Symbol] = None,
+                        mode='differential',
+                        method='sqrt-count-first',
+                        debug=None) -> List[sp.Eq]:
+    """
+        Transforms the system into quadratic-linear form using variable replacement technique.
+
+        :param system: system of differential equations in form x' = f(x)
+        :param parameter_variables: constant parameter variables
+        :param input_variables: variables representing input functions
+        :param mode: auxiliary equation form.
+        :param method: next replacement choice method.
+
+        Mode
+        -----------------
+        **algebraic**
+            adds auxiliary equations in form y = f(x, y)
+        **differential**
+             adds auxiliary equations in form y' = f(x, y)
+
+        Method
+        -----------------
+        **random**
+            choose next possible replacement in random way;
+        **count-first**
+            choose most frequent possible replacement as the next one;
+        **sqrt-first**
+            choose next possible replacement within variables' squares in random way;
+        **sqrt-count-first**
+             choose most frequent square replacement as the next one;
+
+
+    """
+    eq_system = EquationSystem(system, parameter_variables, input_variables)
+    if not eq_system.is_polynomial('full'):
+        eq_system.polynomialize(mode)
+    eq_system.quadratic_linearize(mode, method, debug)
+    return eq_system.equations
