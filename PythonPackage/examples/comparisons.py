5cd365cdd07cae818fc6b0035261780a11366db1
AndreyBychkov <abychkov@edu.hse.ru>
2020-05-10 01:51:34 +0300

    Update examples

--- PythonPackage/examples/comparisons.py
+++ PythonPackage/examples/comparisons.py
@@ -10,21 +10,21 @@ system = EquationSystem([
     sp.Eq(dot_x, x / (1 + sp.exp(x)))
 ])
 
-system.polynomialize()
+system = polynomialize(system)
 
 
 def ql_bfs_stat(_) -> int:
-    ql_system = system.quadratic_linearized(mode="optimal", method_optimal="bfs")
+    ql_system = quadratic_linearize(system, mode="optimal", method_optimal="bfs")
     return ql_system.statistics.steps
 
 
 def ql_iddfs_1_stat(_) -> int:
-    ql_system = system.quadratic_linearized(mode="optimal", method_optimal="iddfs",
-                                            initial_max_depth=1, heuristics='replacement-value')
+    ql_system = quadratic_linearize(system, mode="optimal", method_optimal="iddfs",
+                                    initial_max_depth=1, heuristics='replacement-value')
     return ql_system.statistics.steps
 
 
 def ql_iddfs_2_stat(_) -> int:
-    ql_system = system.quadratic_linearized(mode="optimal", method_optimal="iddfs",
-                                            initial_max_depth=2, heuristics='replacement-value')
+    ql_system = quadratic_linearize(system, mode="optimal", method_optimal="iddfs",
+                                    initial_max_depth=2, heuristics='replacement-value')
     return ql_system.statistics.steps
