3b0564566e48848584a27357132282f92fe6764b
AndreyBychkov <abychkov@edu.hse.ru>
2020-05-10 18:49:59 +0300

    1) Add sorting heuristics functions 2) Add summary monomial degree heuristics 3) Refactor

--- PythonPackage/replacement_heuristics.py
+++ PythonPackage/replacement_heuristics.py
@@ -4,10 +4,14 @@ import random as rand
 
 from structures import *
 from functools import partial
-from typing import List
-from collections import Counter
-
+from typing import List, Tuple
 from util import is_monomial_divisor
+
+
+def random_sorted(system: EquationSystem) -> Tuple[sp.Poly]:
+    possible_replacements = system.get_possible_replacements(count_sorted=False)
+    rand.shuffle(possible_replacements)
+    return possible_replacements
 
 
 def random(system: EquationSystem) -> sp.Expr:
@@ -16,56 +20,48 @@ def random(system: EquationSystem) -> sp.Expr:
     return rand_replacement
 
 
-def count_first(system: EquationSystem) -> sp.Expr:
-    possible_replacements = system.get_possible_replacements(True)
-    most_frequent_replacement = possible_replacements[0].as_expr()
+def frequent_first_sorted(system: EquationSystem) -> Tuple[sp.Poly]:
+    return system.get_possible_replacements(count_sorted=True)
+
+
+def frequent_first(system: EquationSystem) -> sp.Expr:
+    most_frequent_replacement = frequent_first_sorted(system)[0].as_expr()
     return most_frequent_replacement
 
 
-def sqrt_first(system: EquationSystem) -> sp.Expr:
+def free_variables_count_sorted(system: EquationSystem) -> Tuple[sp.Poly]:
     possible_replacements = system.get_possible_replacements(count_sorted=False)
-    sqrt_replacements = tuple(filter(lambda x: len(x.free_symbols) == 1, possible_replacements))
-    if sqrt_replacements:
-        return sqrt_replacements[0].as_expr()
-    else:
-        return possible_replacements[0].as_expr()
+    return tuple(sorted(possible_replacements, key=lambda x: len(x.free_symbols)))
 
 
-def sqrt_count_first(system: EquationSystem) -> sp.Expr:
-    possible_replacements = system.get_possible_replacements(count_sorted=True)
-    sqrt_replacements = tuple(filter(lambda x: len(x.free_symbols) == 1, possible_replacements))
-    if sqrt_replacements:
-        return sqrt_replacements[0].as_expr()
-    else:
-        return possible_replacements[0].as_expr()
+def free_variables_count(system: EquationSystem) -> sp.Expr:
+    return free_variables_count_sorted(system)[0].as_expr()
 
 
-def max_replacement_value(system: EquationSystem) -> sp.Expr:
-    return max_replacement_value_list(system)[0].as_expr()
-
-
-def max_replacement_value_list(system: EquationSystem) -> List[sp.Poly]:
-    system.update_poly_degrees()  # Optimization can be performed here
+def auxiliary_equation_degree_sorted(system: EquationSystem) -> List[sp.Poly]:
+    system.update_poly_degrees()
     possible_replacements = system.get_possible_replacements(count_sorted=False)
-    value_sorted_replacements = sorted(possible_replacements, key=partial(_compute_replacement_value, system))
-    return value_sorted_replacements
+    return sorted(possible_replacements, key=partial(_compute_auxiliary_equation_degree, system))
 
 
-def _compute_replacement_value(system: EquationSystem, replacement: sp.Poly) -> int:
+def auxiliary_equation_degree(system: EquationSystem) -> sp.Expr:
+    return auxiliary_equation_degree_sorted(system)[0].as_expr()
+
+
+def _compute_auxiliary_equation_degree(system: EquationSystem, replacement: sp.Poly) -> int:
     used_equations_subs = list(map(make_derivative_symbol, replacement.free_symbols))
     subs_degrees = list(map(lambda subs: system._equations_poly_degrees[subs], used_equations_subs))
-    auxiliary_equation_degree = max(subs_degrees)
 
-    return auxiliary_equation_degree
+    return max(subs_degrees)
+
+
+def summary_monomial_degree_sorted(system: EquationSystem) -> List[sp.Poly]:
+    possible_replacements = system.get_possible_replacements(count_sorted=False)
+    return sorted(possible_replacements, key=partial(_compute_replacement_value_for_all_monomials, system), reverse=True)
 
 
 def summary_monomial_degree(system: EquationSystem) -> sp.Expr:
     return summary_monomial_degree(system)[0].as_expr()
-
-
-def summary_monomial_degree_list(system: EquationSystem) -> List[sp.Poly]:
-    possible_replacements = system.get_possible_replacements(count_sorted=False)
-    return sorted(possible_replacements, key=partial(_compute_replacement_value_for_all_monomials, system), reverse=False)
 
 
 def _compute_monomials_affected(system: EquationSystem, replacement: sp.Poly, unique=False) -> int:
@@ -77,17 +73,27 @@ def _compute_monomials_affected(system: EquationSystem, replacement: sp.Poly, un
 
 
 def _compute_replacement_value_for_all_monomials(system: EquationSystem, replacement: sp.Poly) -> int:
-    return replacement.total_degree() * _compute_monomials_affected(system, replacement)
+    return (replacement.total_degree() - 1) * _compute_monomials_affected(system, replacement)
 
 
 _heuristics_name_to_function = \
     {
-        'random'                 : random,
-        'sqrt-first'             : sqrt_first,
-        'sqrt-count-first'       : sqrt_count_first,
-        'replacement-value'      : max_replacement_value,
-        'summary-monomial-degree': summary_monomial_degree,
-        'default'                : max_replacement_value
+        'random'                   : random,
+        'frequent-first'           : frequent_first,
+        'free-variables-count'     : free_variables_count,
+        'auxiliary-equation-degree': auxiliary_equation_degree,
+        'summary-monomial-degree'  : summary_monomial_degree,
+        'default'                  : summary_monomial_degree
+    }
+
+_heuristics_name_to_sorter = \
+    {
+        'random'                   : random_sorted,
+        'frequent-first'           : frequent_first_sorted,
+        'free-variables-count'     : free_variables_count_sorted,
+        'auxiliary-equation-degree': auxiliary_equation_degree_sorted,
+        'summary-monomial-degree'  : summary_monomial_degree_sorted,
+        'default'                  : summary_monomial_degree_sorted
     }
 
 
@@ -97,3 +103,11 @@ def get_heuristics(name: str):
     except KeyError:
         sys.stderr.write("Your heuristics has wrong name. Used default heuristics.")
         return _heuristics_name_to_function['default']
+
+
+def get_heuristic_sorter(name: str):
+    try:
+        return _heuristics_name_to_sorter[name]
+    except KeyError:
+        sys.stderr.write("Your heuristics has wrong name. Used default heuristics.")
+        return _heuristics_name_to_sorter['default']
--- PythonPackage/tests/test_replacement_heuristics.py
+++ PythonPackage/tests/test_replacement_heuristics.py
@@ -9,6 +9,15 @@ x, y, z = sp.symbols('x, y, z')
 x_dot, y_dot, z_dot = list(map(make_derivative_symbol, [x, y, z]))
 
 
+def test_free_variables_count_sorted():
+    system = EquationSystem([
+        sp.Eq(x_dot, x * y**2)
+    ])
+
+    replacements = tuple(map(sp.Poly.as_expr, free_variables_count_sorted(system)))
+    assert replacements == (y**2, x*y)
+
+
 def test_compute_monomials_affected():
     system = EquationSystem([
         sp.Eq(x_dot, x ** 2 + x ** 2 * y ** 2),
@@ -22,6 +31,7 @@ def test_compute_monomials_affected():
     replacement = sp.Poly(x ** 3 * y ** 3)
     assert _compute_monomials_affected(system, replacement) == 0
 
+
 def test_compute_replacement_value_for_all_monomials():
     system = EquationSystem([
         sp.Eq(x_dot, x ** 2 + x ** 2 * y ** 2),
@@ -29,7 +39,7 @@ def test_compute_replacement_value_for_all_monomials():
     ])
     replacement = sp.Poly(x * y)
 
-    assert _compute_replacement_value_for_all_monomials(system, replacement) == 6
+    assert _compute_replacement_value_for_all_monomials(system, replacement) == 3
 
     replacement = sp.Poly(x ** 3 * y ** 3)
     assert _compute_monomials_affected(system, replacement) == 0
