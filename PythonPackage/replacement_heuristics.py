2c153c3cec54a34bf2db91eb8c284021b369ada4
AndreyBychkov <abychkov@edu.hse.ru>
2020-05-11 02:12:51 +0300

    Update documentation for heuristics

--- PythonPackage/replacement_heuristics.py
+++ PythonPackage/replacement_heuristics.py
@@ -1,3 +1,7 @@
+"""
+Heuristics for choosing a monomial replacement for quadratic linearization.
+"""
+
 import sys
 import sympy as sp
 import random as rand
@@ -9,42 +13,50 @@ from util import is_monomial_divisor
 
 
 def random_sorted(system: EquationSystem) -> Tuple[sp.Poly]:
+    """Shuffles monomial replacements"""
     possible_replacements = system.get_possible_replacements(count_sorted=False)
     rand.shuffle(possible_replacements)
     return possible_replacements
 
 
 def random(system: EquationSystem) -> sp.Expr:
+    """Picks a random monomial replacement"""
     possible_replacements = system.get_possible_replacements(count_sorted=False)
     rand_replacement = rand.choice(possible_replacements).as_expr()
     return rand_replacement
 
 
 def frequent_first_sorted(system: EquationSystem) -> Tuple[sp.Poly]:
+    """The most frequent monomial replacements are at the beginning"""
     return system.get_possible_replacements(count_sorted=True)
 
 
 def frequent_first(system: EquationSystem) -> sp.Expr:
+    """Choose the most frequent monomial replacement"""
     most_frequent_replacement = frequent_first_sorted(system)[0].as_expr()
     return most_frequent_replacement
 
 
 def free_variables_count_sorted(system: EquationSystem) -> Tuple[sp.Poly]:
+    """Monomial replacements with fewer variables are at the beginning"""
     possible_replacements = system.get_possible_replacements(count_sorted=False)
     return tuple(sorted(possible_replacements, key=lambda x: len(x.free_symbols)))
 
 
 def free_variables_count(system: EquationSystem) -> sp.Expr:
+    """Choose a monomial replacement with the least free variables"""
     return free_variables_count_sorted(system)[0].as_expr()
 
 
 def auxiliary_equation_degree_sorted(system: EquationSystem) -> List[sp.Poly]:
+    """Monomial replacements with lower generated auxiliary equation degree are at the beginning"""
     system.update_poly_degrees()
     possible_replacements = system.get_possible_replacements(count_sorted=False)
     return sorted(possible_replacements, key=partial(_compute_auxiliary_equation_degree, system))
 
 
 def auxiliary_equation_degree(system: EquationSystem) -> sp.Expr:
+    """Choose a monomial replacement with the least generated auxiliary degree"""
     return auxiliary_equation_degree_sorted(system)[0].as_expr()
 
 
@@ -56,12 +68,14 @@ def _compute_auxiliary_equation_degree(system: EquationSystem, replacement: sp.P
 
 
 def auxiliary_equation_ql_discrepancy_sorted(system: EquationSystem) -> List[sp.Poly]:
+    """Monomial replacements which generated auxiliary equation are closer to quadratic form are at the beginning"""
     system.update_poly_degrees()
     possible_replacements = system.get_possible_replacements(count_sorted=False)
     return sorted(possible_replacements, key=partial(_compute_auxiliary_equation_ql_discrepancy, system))
 
 
 def auxiliary_equation_ql_discrepancy(system: EquationSystem) -> sp.Expr:
+    """Choose a monomial replacement which generated auxiliary equation is closest to quadratic form"""
     return auxiliary_equation_ql_discrepancy_sorted(system)[0].as_expr()
 
 
@@ -73,11 +87,13 @@ def _compute_auxiliary_equation_ql_discrepancy(system: EquationSystem, replaceme
 
 
 def summary_monomial_degree_sorted(system: EquationSystem) -> List[sp.Poly]:
+    """Monomial replacements which strongly reduce the dimension of the system are at the begging"""
     possible_replacements = system.get_possible_replacements(count_sorted=False)
     return sorted(possible_replacements, key=partial(_compute_replacement_value_for_all_monomials, system), reverse=True)
 
 
 def summary_monomial_degree(system: EquationSystem) -> sp.Expr:
+    """Choose a monomial replacement with maximal reduction of system's dimension"""
     return summary_monomial_degree(system)[0].as_expr()
 
 
