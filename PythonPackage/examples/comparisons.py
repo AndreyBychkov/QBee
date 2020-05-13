51ab5fb9dd7b01a273f6d8cb0f054c62a15f9fc1
AndreyBychkov <abychkov@edu.hse.ru>
2020-05-13 20:20:02 +0300

    Update file

--- PythonPackage/examples/comparisons.py
+++ PythonPackage/examples/comparisons.py
@@ -4,7 +4,7 @@ from qbee import *
 sp.init_printing()
 
 x = sp.symbols('x')
-dot_x = make_derivative_symbol(x)
+dot_x = derivatives(x)
 
 system = EquationSystem([
     sp.Eq(dot_x, x / (1 + sp.exp(x)))
@@ -19,12 +19,10 @@ def ql_bfs_stat(_) -> int:
 
 
 def ql_iddfs_1_stat(_) -> int:
-    ql_system = quadratic_linearize(system, mode="optimal", method_optimal="iddfs",
-                                    initial_max_depth=1, heuristics='replacement-value')
+    ql_system = quadratic_linearize(system, mode="optimal", heuristics='default', method_optimal="iddfs", initial_max_depth=1)
     return ql_system.statistics.steps
 
 
 def ql_iddfs_2_stat(_) -> int:
-    ql_system = quadratic_linearize(system, mode="optimal", method_optimal="iddfs",
-                                    initial_max_depth=2, heuristics='replacement-value')
+    ql_system = quadratic_linearize(system, mode="optimal", heuristics='default', method_optimal="iddfs", initial_max_depth=2)
     return ql_system.statistics.steps
