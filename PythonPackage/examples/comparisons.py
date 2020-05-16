8ac3579e9325fd234c16c21c2fc0c1e0dddbd7e2
AndreyBychkov <abychkov@edu.hse.ru>
2020-05-17 02:39:35 +0300

    Update examples

--- PythonPackage/examples/comparisons.py
+++ PythonPackage/examples/comparisons.py
@@ -14,15 +14,15 @@ system = polynomialize(system)
 
 
 def ql_bfs_stat(_) -> int:
-    ql_system = quadratic_linearize(system, mode="optimal", method_optimal="bfs", limit_depth=2)
-    return ql_system.statistics.steps
+    ql_result = quadratic_linearize(system, mode="optimal", method_optimal="bfs", limit_depth=2)
+    return ql_result.statistics.steps
 
 
 def ql_iddfs_1_stat(_) -> int:
-    ql_system = quadratic_linearize(system, mode="optimal", heuristics='default', method_optimal="iddfs", initial_max_depth=1, limit_depth=2)
-    return ql_system.statistics.steps
+    ql_result = quadratic_linearize(system, mode="optimal", heuristics='default', method_optimal="iddfs", initial_max_depth=1, limit_depth=2)
+    return ql_result.statistics.steps
 
 
 def ql_iddfs_2_stat(_) -> int:
-    ql_system = quadratic_linearize(system, mode="optimal", heuristics='default', method_optimal="iddfs", initial_max_depth=2, limit_depth=2)
-    return ql_system.statistics.steps
+    ql_result = quadratic_linearize(system, mode="optimal", heuristics='default', method_optimal="iddfs", initial_max_depth=2, limit_depth=2)
+    return ql_result.statistics.steps
