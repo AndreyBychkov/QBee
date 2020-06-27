679767971986bd242fb37f7ba1c75be0b0d166d1
AndreyBychkov <abychkov@edu.hse.ru>
2020-06-28 01:04:58 +0300

    Replace 'ID-DFS' with 'ID-DLS'

--- examples/xSigmoid/xSigmoid.py
+++ examples/xSigmoid/xSigmoid.py
@@ -18,32 +18,32 @@ def quad_bfs_stat(_) -> int:
     return quad_result.statistics.steps
 
 
-def quad_iddfs_random(_) -> int:
-    quad_result = quadratize(system, method_optimal="iddfs", heuristics='random', initial_max_depth=2, limit_depth=2)
+def quad_iddls_random(_) -> int:
+    quad_result = quadratize(system, method_optimal="iddls", heuristics='random', initial_max_depth=2, limit_depth=2)
     return quad_result.statistics.steps
 
 
-def quad_iddfs_frequent_first(_) -> int:
-    quad_result = quadratize(system, method_optimal="iddfs", heuristics='frequent-first', initial_max_depth=2, limit_depth=2)
+def quad_iddls_frequent_first(_) -> int:
+    quad_result = quadratize(system, method_optimal="iddls", heuristics='frequent-first', initial_max_depth=2, limit_depth=2)
     return quad_result.statistics.steps
 
 
-def quad_iddfs_free_variables_count(_) -> int:
-    quad_result = quadratize(system, method_optimal="iddfs", heuristics='free-variables-count', initial_max_depth=2, limit_depth=2)
+def quad_iddls_free_variables_count(_) -> int:
+    quad_result = quadratize(system, method_optimal="iddls", heuristics='free-variables-count', initial_max_depth=2, limit_depth=2)
     return quad_result.statistics.steps
 
 
-def quad_iddfs_auxiliary_equation_degree(_) -> int:
-    quad_result = quadratize(system, method_optimal="iddfs", heuristics='auxiliary-equation-degree', initial_max_depth=2, limit_depth=2)
+def quad_iddls_auxiliary_equation_degree(_) -> int:
+    quad_result = quadratize(system, method_optimal="iddls", heuristics='auxiliary-equation-degree', initial_max_depth=2, limit_depth=2)
     return quad_result.statistics.steps
 
 
-def quad_iddfs_auxiliary_equation_quadratic_discrepancy(_) -> int:
-    quad_result = quadratize(system, method_optimal="iddfs", heuristics='auxiliary-equation-quadratic-discrepancy', initial_max_depth=2,
+def quad_iddls_auxiliary_equation_quadratic_discrepancy(_) -> int:
+    quad_result = quadratize(system, method_optimal="iddls", heuristics='auxiliary-equation-quadratic-discrepancy', initial_max_depth=2,
                            limit_depth=2)
     return quad_result.statistics.steps
 
 
-def quad_iddfs_summary_monomial_degree(_) -> int:
-    quad_result = quadratize(system, method_optimal="iddfs", heuristics='summary-monomial-degree', initial_max_depth=2, limit_depth=2)
+def quad_iddls_summary_monomial_degree(_) -> int:
+    quad_result = quadratize(system, method_optimal="iddls", heuristics='summary-monomial-degree', initial_max_depth=2, limit_depth=2)
     return quad_result.statistics.steps
