8b10a94e0bf0ace7b081df545a97298f7867b350
AndreyBychkov <abychkov@edu.hse.ru>
2020-11-01 02:49:55 +0300

    Introduced OO, move out heuristics

--- qbee/bb.py
+++ qbee/bb.py
@@ -1,9 +1,14 @@
 import copy
-import functools as ft
 import math
 import time
 
 from sympy import *
+from sympy.polys.monomials import monomial_deg
+from typing import Callable, Union, Collection, Optional, List, Tuple
+from functools import reduce
+from operator import add
+from util import monomial_to_poly
+
 
 def get_decompositions(monomial):
     if len(monomial) == 0:
@@ -15,14 +20,25 @@ def get_decompositions(monomial):
             a, b = tuple(list(r[0]) + [i]), tuple(list(r[1]) + [monomial[-1] - i])
             result.add((min(a, b), max(a, b)))
     return result
-        
-class PartialResult:
-    def __init__(self, polynomials):
+
+
+def default_score(system) -> int:
+    total_nonsquare = sum([monomial_deg(m) for m in system.nonsquares])
+    return total_nonsquare + system.dim * len(system.vars)
+
+
+def _mlist_to_poly(mlist: Collection[Monomial], gens) -> Poly:
+    return Poly(reduce(add, mlist), gens)
+
+
+class PolynomialSystem:
+    def __init__(self, polynomials: List[Poly]):
         """
         polynomials - right-hand sides of the ODE system listed in the same order as 
                       the variables in the polynomial ring
         """
         self.dim = len(polynomials[0].ring.gens)
+        self.gen_syms = list(map(lambda g: Symbol(str(g)), polynomials[0].ring.gens))
 
         # put not monomials but differents in the exponents between rhs and lhs
         self.rhs = dict()
@@ -52,14 +68,10 @@ class PartialResult:
     def is_quadratized(self):
         return not self.nonsquares
 
-    def get_system_score(self):
-        total_nonsquare = sum([sum(m) for m in self.nonsquares])
-        return total_nonsquare + self.dim * len(self.vars)
-
     def get_smallest_nonsquare(self):
         return min([(sum(m), m) for m in self.nonsquares])[1]
 
-    def next_generation(self):
+    def next_generation(self, heuristics=default_score):
         new_gen = []
         for d in get_decompositions(self.get_smallest_nonsquare()):
             c = copy.deepcopy(self)
@@ -67,10 +79,103 @@ class PartialResult:
                 c.add_var(v)
             new_gen.append(c)
 
-        return sorted(new_gen, key=lambda s: s.get_system_score())
+        return sorted(new_gen, key=heuristics)
 
     def new_vars_count(self):
-        return len(self.vars) - self.dim  - 1
+        return len(self.vars) - self.dim - 1
+
+    def apply_quadratizaton(self):
+        # TODO
+        new_variables = list(filter(lambda m: monomial_deg(m) >= 2, self.vars))
+
+    def to_sympy(self, gens):
+        # TODO: Does not work correctly with PolyElement
+        return [_mlist_to_poly(mlist, gens) for mlist in self.rhs.values()]
+
+
+Heuristics = Callable[[PolynomialSystem], int]
+TerminationCriteria = Callable[[PolynomialSystem], bool]
+
+
+class QuadratizationResult:
+    def __init__(self,
+                 system: PolynomialSystem,
+                 introduced_vars: int,
+                 nodes_traversed: int):
+        self.system = system
+        self.introduced_vars = introduced_vars
+        self.nodes_traversed = nodes_traversed
+
+    def sympy_str(self, variables):
+        pass
+
+    def __repr__(self):
+        return f"Number of introduced variables: {self.introduced_vars}\n" +\
+               f"Introduced variables: {self._intoroduced_variables_str()}\n" +\
+               f"Nodes traversed: {self.nodes_traversed}"
+
+    def _intoroduced_variables_str(self):
+        return list(map(lambda v: latex(monomial_to_poly(Monomial(v, self.system.gen_syms)).as_expr()),
+                        self._remove_free_variables(self.system.vars)))
+
+    def _remove_free_variables(self, vars: Collection[tuple]):
+        return tuple(filter(lambda v: monomial_deg(v) >= 2, vars))
+
+    def _var_to_symbolic(self, v: tuple):
+        return ''.join([f"{g}^{p}" for g, p in zip(self.system.gen_syms, v)])
+
+
+class Algorithm:
+    def __init__(self,
+                 poly_system: PolynomialSystem,
+                 heuristics: Heuristics = default_score,
+                 termination_criteria: Union[TerminationCriteria, Collection[TerminationCriteria]] = None):
+        self._system = poly_system
+        self._heuristics = heuristics
+        self._early_termination_funs = list(termination_criteria) if termination_criteria is not None else [
+            lambda _: False, ]
+
+    def quadratize(self) -> QuadratizationResult:
+        pass
+
+    def attach_early_termimation(self, termination_criteria: Callable[[PolynomialSystem], bool]) -> None:
+        self._early_termination_funs.append(termination_criteria)
+
+    @property
+    def heuristics(self):
+        return self._heuristics
+
+    @heuristics.setter
+    def heuristics(self, value):
+        self._heuristics = value
+
+
+class BranchAndBound(Algorithm):
+    def __init__(self, poly_system: PolynomialSystem, upper_bound: int):
+        super().__init__(poly_system)
+        self.upper_bound = upper_bound
+
+    def quadratize(self) -> QuadratizationResult:
+        nvars, opt_system, traversed = self._bnb_step(self._system, self.upper_bound)
+        return QuadratizationResult(opt_system, nvars, traversed)
+
+    def _bnb_step(self, part_res: PolynomialSystem, best_nvars) -> Tuple[
+        Union[int, float], Optional[PolynomialSystem], int]:
+        if part_res.is_quadratized():
+            return part_res.new_vars_count(), part_res, 1
+        if part_res.new_vars_count() >= best_nvars - 1:
+            return math.inf, None, 1
+
+        traversed_total = 1
+        min_nvars, best_system = best_nvars, None
+        for next_system in part_res.next_generation():
+            nvars, opt_system, traversed = find_optimal_quadratization(next_system, min_nvars)
+            traversed_total += traversed
+            if nvars < min_nvars:
+                min_nvars = nvars
+                best_system = opt_system
+        return min_nvars, best_system, traversed_total
+
 
 def find_optimal_quadratization(part_res, best=math.inf):
     if part_res.is_quadratized():
@@ -88,10 +193,11 @@ def find_optimal_quadratization(part_res, best=math.inf):
             best_system = opt_system
     return (min_nvars, best_system, traversed_total)
 
-if __name__ == "__main__":
+
+def test_systems():
     R, x, y = ring(["x", "y"], QQ)
 
-    system = PartialResult([x**2 + y, 3 * x * y**3 - y])
+    system = PolynomialSystem([x ** 2 + y, 3 * x * y ** 3 - y])
     start = time.time()
     nvars, sys, nodes = find_optimal_quadratization(system)
     end = time.time()
@@ -101,7 +207,7 @@ if __name__ == "__main__":
     print(f"Number of nodes traversed by the algo {nodes}")
     print("===========")
 
-    system = PartialResult([(x + 1)**8, y])
+    system = PolynomialSystem([(x + 1) ** 8, y])
     start = time.time()
     nvars, sys, nodes = find_optimal_quadratization(system)
     end = time.time()
@@ -114,7 +220,7 @@ if __name__ == "__main__":
     print(f"Number of nodes traversed by the algo {nodes}")
     print("===========")
 
-    system = PartialResult([(x + 1)**15, y])
+    system = PolynomialSystem([(x + 1) ** 15, y])
     start = time.time()
     nvars, sys, nodes = find_optimal_quadratization(system)
     end = time.time()
@@ -129,7 +235,7 @@ if __name__ == "__main__":
 
     # xSigmoid
     R, x, y, z = ring(["x", "y", "z"], QQ)
-    system = PartialResult([x * z, x * y * z, x * y * z**3])
+    system = PolynomialSystem([x * z, x * y * z, x * y * z ** 3])
     start = time.time()
     nvars, sys, nodes = find_optimal_quadratization(system)
     end = time.time()
@@ -144,7 +250,7 @@ if __name__ == "__main__":
 
     # Rabinovich-Fabrikant
     R, x, y, z = ring(["x", "y", "z"], QQ)
-    system = PartialResult([y * (z - 1 - x**2) + x, x * (3 * z + 1 - x**2) + y, -2 * z * (2 + x * y)])
+    system = PolynomialSystem([y * (z - 1 - x ** 2) + x, x * (3 * z + 1 - x ** 2) + y, -2 * z * (2 + x * y)])
     start = time.time()
     nvars, sys, nodes = find_optimal_quadratization(system)
     end = time.time()
@@ -155,5 +261,16 @@ if __name__ == "__main__":
     print(f"Order of quadratization {nvars}")
     print(f"All the variables {sys.vars}")
     print(f"Number of nodes traversed by the algo {nodes}")
-    print("===========")   
- 
+    print("===========")
+
+
+def general_test():
+    R, x, y = ring(['x', 'y'], QQ)
+    poly_system = PolynomialSystem([x ** 2 + y, 3 * x * y ** 3 - y])
+    algo = BranchAndBound(poly_system, 5)
+    res = algo.quadratize()
+    print(res)
+
+
+if __name__ == "__main__":
+    general_test()
