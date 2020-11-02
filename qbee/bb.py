76ce1bac0859eb610b39e687870aab0b093c4f90
AndreyBychkov <abychkov@edu.hse.ru>
2020-11-02 17:39:13 +0300

    Add new tests

--- qbee/bb.py
+++ qbee/bb.py
@@ -10,6 +10,7 @@ from functools import reduce
 from operator import add
 from util import monomial_to_poly
 from tqdm import tqdm
+from collections import deque
 
 
 def timed(func):
@@ -301,22 +302,40 @@ def simple_test():
     res = algo.quadratize()
     print(res)
 
+
 @timed
 def poly_test():
     R, x, y = ring(['x', 'y'], QQ)
-    poly_system = PolynomialSystem([(x + 1)**8, y])
+    poly_system = PolynomialSystem([(x + 1) ** 8, y])
+    algo = BranchAndBound(poly_system, 5, heuristics=default_score)
+    res = algo.quadratize()
+    print(res)
+
+
+@timed
+def long_poly_test():
+    R, x, y = ring(['x', 'y'], QQ)
+    poly_system = PolynomialSystem([(x + 1) ** 15, y])
+    algo = BranchAndBound(poly_system, 8, heuristics=aeqd_score)
+    res = algo.quadratize()
+    print(res)
+
+@timed
+def xSigmoid():
+    R, x, y, z = ring(["x", "y", "z"], QQ)
+    poly_system = PolynomialSystem([x * z, x * y * z, x * y * z ** 3])
     algo = BranchAndBound(poly_system, 5, heuristics=default_score)
     res = algo.quadratize()
     print(res)
 
 @timed
-def long_poly_test():
-    R, x, y = ring(['x', 'y'], QQ)
-    poly_system = PolynomialSystem([(x + 1)**15, y])
-    algo = BranchAndBound(poly_system, 8, heuristics=aeqd_score)
+def RabinovichFabricant():
+    R, x, y, z = ring(["x", "y", "z"], QQ)
+    poly_system = PolynomialSystem([y * (z - 1 - x ** 2) + x, x * (3 * z + 1 - x ** 2) + y, -2 * z * (2 + x * y)])
+    algo = BranchAndBound(poly_system, 4, heuristics=default_score)
     res = algo.quadratize()
     print(res)
 
 
 if __name__ == "__main__":
-    long_poly_test()
+    RabinovichFabricant()
