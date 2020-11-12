7c52c667c167318d718c91b3cd1e31c4f5567755
AndreyBychkov <abychkov@edu.hse.ru>
2020-11-13 02:00:36 +0300

    Add assertion to tests

--- tests/test_quadratization.py
+++ tests/test_quadratization.py
@@ -9,6 +9,8 @@ def test_simple():
     poly_system = PolynomialSystem([x ** 2 + y, 3 * x * y ** 3 - y])
     algo = BranchAndBound(poly_system, 10, heuristics=default_score)
     res = algo.quadratize()
+    assert res.system.is_quadratized()
+    assert res.introduced_vars <= 2
     print(res)
 
 
@@ -18,6 +20,8 @@ def test_poly():
     poly_system = PolynomialSystem([(x + 1) ** 8, y])
     algo = BranchAndBound(poly_system, 20, heuristics=default_score)
     res = algo.quadratize()
+    assert res.system.is_quadratized()
+    assert res.introduced_vars <= 4
     print(res)
 
 
@@ -27,6 +31,8 @@ def test_poly_long():
     poly_system = PolynomialSystem([(x + 1) ** 15, y])
     algo = BranchAndBound(poly_system, 8, heuristics=default_score)
     res = algo.quadratize()
+    assert res.system.is_quadratized()
+    assert res.introduced_vars <= 7
     print(res)
 
 
@@ -36,6 +42,8 @@ def test_xSigmoid():
     poly_system = PolynomialSystem([x * z, x * y * z, x * y * z ** 3])
     algo = BranchAndBound(poly_system, 5, heuristics=default_score)
     res = algo.quadratize()
+    assert res.system.is_quadratized()
+    assert res.introduced_vars <= 2
     print(res)
 
 
@@ -45,4 +53,6 @@ def test_RabinovichFabricant():
     poly_system = PolynomialSystem([y * (z - 1 - x ** 2) + x, x * (3 * z + 1 - x ** 2) + y, -2 * z * (2 + x * y)])
     algo = BranchAndBound(poly_system, 20, heuristics=default_score)
     res = algo.quadratize()
+    assert res.system.is_quadratized()
+    assert res.introduced_vars <= 3
     print(res)
