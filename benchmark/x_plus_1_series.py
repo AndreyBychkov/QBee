8084c795d39c0a864b1ad2674f8d4994a9fd5d70
AndreyBychkov <abychkov@edu.hse.ru>
2020-11-26 03:44:40 +0300

    Add termination funcs to tests and general update

--- benchmark/x_plus_1_series.py
+++ benchmark/x_plus_1_series.py
@@ -2,21 +2,23 @@ import pytest
 import sympy as sp
 from qbee import *
 from qbee.heuristics import *
+from qbee.quadratization import termination_by_square_bound, termination_by_best_nvars, termination_by_C4_bound
 
 R, x = sp.ring(["x"], sp.QQ)
 
 heuristics = [default_score, aeqd_score, smd_score]
-systems = dict([(d, PolynomialSystem([(x + 1) ** d])) for d in range(6, 15)])
+terminations = [termination_by_square_bound, termination_by_best_nvars, termination_by_C4_bound]
+systems = dict([(d, PolynomialSystem([(x + 1) ** d])) for d in range(6, 9)])
 order = systems.keys()
 
 
-def quadratize(heuristics: Heuristics, order):
-    algo = BranchAndBound(systems[order], heuristics=heuristics)
+def quadratize(order, heuristics: Heuristics, termination):
+    algo = BranchAndBound(systems[order], heuristics=heuristics, early_termination=[termination])
     algo.quadratize()
 
-
+@pytest.mark.parametrize('term', terminations)
 @pytest.mark.parametrize('heur', heuristics)
 @pytest.mark.parametrize('ord', order)
-def test_sigmoid(benchmark, heur, ord):
-    benchmark(quadratize, heuristics=heur, order=ord)
+def test_sigmoid(benchmark, ord, heur, term):
+    benchmark(quadratize, order=ord, heuristics=heur, termination=term)
 
