ccdb615322e7e8782b4abdf93a57b03cfc37a034
AndreyBychkov <abychkov@edu.hse.ru>
2021-01-19 02:15:01 +0300

    Expanded main

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import pickle
 from qbee.examples import *
+
 
 def x6x4x3():
     R, x = sp.ring('x', sp.QQ)
     system = PolynomialSystem([x ** 6 + x ** 4 + x ** 3])
     algo = BranchAndBound(system, aeqd_score, [termination_by_best_nvars])
     results = algo.get_optimal_quadratizations()
-    pickle.dump(results, open('../log/quad_systems.pkl', 'wb'))
     for res in results:
         print(res)
+
 
 def circ4():
     system = generate_circular(4)
@@ -18,5 +18,17 @@ def circ4():
         print(res)
 
 
+def hard3():
+    system = generate_hard(3)
+    algo = BranchAndBound(system, aeqd_score, [termination_by_best_nvars])
+    results = algo.get_optimal_quadratizations()
+    for res in results:
+        print(res)
+
+
 if __name__ == '__main__':
-    circ4()
+    reduce(add, range(10000000))  # Warmup
+    system = generate_lifeware_conjecture(3)
+    algo = BranchAndBound(system, aeqd_score, [termination_by_best_nvars])
+    results = algo.quadratize()
+    print(results)
