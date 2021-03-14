484930450613720c00502eb9887f89b977260bef
AndreyBychkov <abychkov@edu.hse.ru>
2021-03-14 03:45:40 +0300

    change naming in pytest benchmarks

--- benchmark/hill.py
+++ benchmark/hill.py
@@ -8,18 +8,18 @@ from qbee.examples import generate_hill
 R, x = sp.ring(["x"], sp.QQ)
 
 heuristics = [default_score, aeqd_score, smd_score]
-terminations = [pruning_by_quadratic_upper_bound, pruning_by_best_nvars, pruning_by_squarefree_graphs]
+pruning_funcs = [pruning_by_quadratic_upper_bound, pruning_by_best_nvars, pruning_by_squarefree_graphs]
 systems = dict([(d, generate_hill(d)) for d in [2, 3, 4, 5, 6, 7, 8, 10, 15]])
 order = systems.keys()
 
 
-def quadratize(order, heuristics: Heuristics, termination):
-    algo = BranchAndBound(systems[order], heuristics=heuristics, early_termination=[termination])
+def quadratize(order, heuristics: Heuristics, pruning):
+    algo = BranchAndBound(systems[order], heuristics=heuristics, pruning_funcs=[pruning])
     algo.quadratize()
 
 
-@pytest.mark.parametrize('term', terminations)
+@pytest.mark.parametrize('pruning', pruning_funcs)
 @pytest.mark.parametrize('heur', heuristics)
 @pytest.mark.parametrize('ord', order)
-def test_hill(benchmark, ord, heur, term):
-    benchmark(quadratize, order=ord, heuristics=heur, termination=term)
+def test_hill(benchmark, ord, heur, pruning):
+    benchmark(quadratize, order=ord, heuristics=heur, termination=pruning)
--- benchmark/long_monomial.py
+++ benchmark/long_monomial.py
@@ -9,18 +9,18 @@ R, x = sp.ring(["x"], sp.QQ)
 
 # heuristics = [default_score, aeqd_score, smd_score]  # Does not really change performance by now
 heuristics = [default_score]
-terminations = [pruning_by_quadratic_upper_bound, pruning_by_best_nvars, pruning_by_squarefree_graphs]
+pruning_funcs = [pruning_by_quadratic_upper_bound, pruning_by_best_nvars, pruning_by_squarefree_graphs]
 systems = dict([(d, generate_lifeware_conjecture(d)) for d in [2, 3]])
 order = systems.keys()
 
 
-def quadratize(order, heuristics: Heuristics, termination):
-    algo = BranchAndBound(systems[order], heuristics=heuristics, early_termination=[termination])
+def quadratize(order, heuristics: Heuristics, pruning):
+    algo = BranchAndBound(systems[order], heuristics=heuristics, pruning_funcs=[pruning])
     algo.quadratize()
 
 
-@pytest.mark.parametrize('term', terminations)
+@pytest.mark.parametrize('pruning', pruning_funcs)
 @pytest.mark.parametrize('heur', heuristics)
 @pytest.mark.parametrize('ord', order)
-def test_long_monomials(benchmark, ord, heur, term):
-    benchmark(quadratize, order=ord, heuristics=heur, termination=term)
+def test_long_monomials(benchmark, ord, heur, pruning):
+    benchmark(quadratize, order=ord, heuristics=heur, pruning=pruning)
