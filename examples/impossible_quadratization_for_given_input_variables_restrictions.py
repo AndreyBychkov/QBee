688be3dbb292f8ddbb9ef6e9c54d0373db244314
AndreyBychkov <abychkov@edu.hse.ru>
2021-10-10 16:37:24 +0300

    Rewrite examples using new API

--- examples/impossible_quadratization_for_given_input_variables_restrictions.py
+++ examples/impossible_quadratization_for_given_input_variables_restrictions.py
@@ -4,24 +4,19 @@ from functools import partial
 from time import time
 
 if __name__ == '__main__':
-    c1, c2, c3, c4 = sp.symbols("c1, c2, c3, c4")
-    A, Ea, Ru = sp.symbols("A, Ea, Ru")
-    T = sp.Symbol("T")
-    dc1, dc2, dc3, dc4 = derivatives([c1, c2, c3, c4])
-    tmp = -A * sp.exp(-Ea / (Ru * T)) * c1 ** 0.2 * c2 ** 1.3
-    system = EquationSystem([
-        sp.Eq(dc1, tmp),
-        sp.Eq(dc2, 2 * tmp),
-        sp.Eq(dc3, -tmp),
-        sp.Eq(dc4, -2 * tmp)],
-        parameter_variables=[A, Ea, Ru],
-        input_variables=[T])
+    c1, c2, c3, c4, T = functions("c1, c2, c3, c4, T")
+    A, Ea, Ru = parameters("A, Ea, Ru")
+    eq1 = -A * sp.exp(-Ea / (Ru * T)) * c1 ** 0.2 * c2 ** 1.3
+    system = [
+        (c1, eq1),
+        (c2, 2 * eq1),
+        (c3, -eq1),
+        (c4, -2 * eq1)
+    ]
 
-    print("Original nonlinear system:")
-    system.print()
-    print("=" * 50)
     timeout = partial(pruning_by_elapsed_time, start_t=time(), max_t=10)
     # {T: 1} means than T can have a derivative of order at most one => T'
-    quadr_system = polynomialize_and_quadratize(system, {T: 1}, pruning_functions=[timeout] + default_pruning_rules)
+    quadr_system = polynomialize_and_quadratize(system, inputs_orders={T: 1},
+                                                pruning_functions=[timeout] + default_pruning_rules)
     if quadr_system:
         print(quadr_system)
--- examples/order_of_input_variables_derivatives.py
+++ examples/order_of_input_variables_derivatives.py
@@ -3,23 +3,17 @@ from qbee import *
 
 
 if __name__ == '__main__':
-    c1, c2, c3, c4 = sp.symbols("c1, c2, c3, c4")
-    A, Ea, Ru = sp.symbols("A, Ea, Ru")
-    T = sp.Symbol("T")
-    dc1, dc2, dc3, dc4 = derivatives([c1, c2, c3, c4])
-    tmp = -A * sp.exp(-Ea / (Ru * T)) * c1 ** 0.2 * c2 ** 1.3
-    system = EquationSystem([
-        sp.Eq(dc1, tmp),
-        sp.Eq(dc2, 2 * tmp),
-        sp.Eq(dc3, -tmp),
-        sp.Eq(dc4, -2 * tmp)],
-        parameter_variables=[A, Ea, Ru],
-        input_variables=[T])
-    print("Original nonlinear system:")
-    system.print()
-    print("=" * 50)
+    c1, c2, c3, c4, T = functions("c1, c2, c3, c4, T")
+    A, Ea, Ru = parameters("A, Ea, Ru")
+    eq1 = -A * sp.exp(-Ea / (Ru * T)) * c1 ** 0.2 * c2 ** 1.3
+    system = [
+        (c1, eq1),
+        (c2, 2 * eq1),
+        (c3, -eq1),
+        (c4, -2 * eq1)
+    ]
+
     # {T: 2} means than T can have a derivative of order at most two => T''
-    quadr_system = polynomialize_and_quadratize(system, {T: 2})
-    print("=" * 50)
+    quadr_system = polynomialize_and_quadratize(system, inputs_orders={T: 2})
     if quadr_system:
         print(quadr_system)
