be3d601e62028711574ceac46390db938adc60fc
AndreyBychkov <abychkov@edu.hse.ru>
2021-10-13 22:11:17 +0300

    Remove myst-parser until we need it

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,6 @@ release = '0.6'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosectionlabel"
