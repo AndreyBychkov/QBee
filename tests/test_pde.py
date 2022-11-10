d060ef2b2dda03ad7a048e87647ac41371c2a696
Andrey Byhckov <abychkov@edu.hse.ru>
2022-11-10 14:51:55 +0300

    Add place for pde tests

---
+++ tests/test_pde.py
@@ -0,0 +1,20 @@
+import pytest
+from functools import partial
+from qbee import *
+
+
+def dym_equations():
+    x = functions("x")
+    u = multivariable_functions("u", [x])
+    rhs = u ** 3 * u.diff(x, 3)
+    system = [
+        (u, rhs),
+        (u.diff(x), rhs.diff(x)),
+        (u.diff(x, 2), rhs.diff(x, 2))
+    ]
+    no_quad_pruning = partial(pruning_by_nodes_without_quadratization_found, nodes_processed=1000)
+    res = polynomialize_and_quadratize(system, pruning_functions=[*default_pruning_rules,
+                                                                  no_quad_pruning
+                                                                  ])
+    assert res is not None
+    print(res)
