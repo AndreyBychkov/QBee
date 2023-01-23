4c26adc7cafa29409bbdfcfdb08b9a4c1481f19c
gpogudin <gpogudin@inria.fr>
2023-01-23 16:00:12 +0100

    allowing no_class variables

--- qbee/selection.py
+++ qbee/selection.py
@@ -20,8 +20,8 @@ Given two mappings from the variables to indices and a list of numbers
 creates a new list in which the values at the indices corresponding to 
 variables in `v_to_ind_from` are placed at positions as prescribed by `v_to_ind_to`
 """
-def _map_indices(v_to_ind_from, v_to_ind_to, arr):
-    result = [0 for _ in arr]
+def _map_indices(v_to_ind_from, v_to_ind_to, arr, no_class):
+    result = [arr[i] if i in no_class else 0 for i in range(len(arr))]
     v_to_val = {v: arr[ind] for v, ind in v_to_ind_from.items()}
     for v, ind in v_to_ind_to.items():
         result[ind] = v_to_val[v]
@@ -34,7 +34,11 @@ def generation_semidiscretized(system):
         system.equivalence_classes = dict()
         system.ind_to_class = dict()
         system.ind_to_var = dict()
+        system.no_class = set()
         for i, s in enumerate(system.gen_symbols):
+            if '_' not in str(s):
+                system.no_class.add(i)
+                continue
             class_ind = int(str(s).split("_")[-1])
             var_name = "_".join(str(s).split("_")[:-1])
             if class_ind not in system.equivalence_classes:
@@ -46,11 +50,13 @@ def generation_semidiscretized(system):
         # building the graph
         system.graph = dict()
         for i, eq in system.rhs.items():
+            if i in system.no_class:
+                continue
             c = system.ind_to_class[i]
             if c not in system.graph:
                 system.graph[c] = set()
             for m in eq:
-                classes = set([system.ind_to_class[j] for j in range(system.dim) if m[j] != 0])
+                classes = set([system.ind_to_class[j] for j in system.ind_to_class.keys() if m[j] != 0])
                 for cl in classes:
                     if cl != c:
                         system.graph[c].add(cl)
@@ -66,21 +72,29 @@ def generation_semidiscretized(system):
     for d in decomposition:
         extended_vars = set(d)
         for v in d:
-            classes = set([system.ind_to_class[j] for j in range(system.dim) if v[j] != 0])
+            classes = set([system.ind_to_class[j] for j in system.ind_to_class.keys() if v[j] != 0])
             assert len(classes) <= 2
             if len(classes) == 1:
                 c = list(classes)[0]
                 for cl, var_to_ind in system.equivalence_classes.items():
-                    extended_vars.add(tuple(_map_indices(system.equivalence_classes[c], var_to_ind, v)))
+                    extended_vars.add(tuple(_map_indices(
+                        system.equivalence_classes[c], var_to_ind, v, system.no_class
+                    )))
             elif len(classes) == 2:
                 c1, c2 = list(classes)
                 for u1, adj in system.graph.items():
                     for u2 in adj:
                         for p in [(c1, c2), (c2, c1)]:
                             if p[1] in system.graph[p[0]]:
-                                m1 = _map_indices(system.equivalence_classes[p[0]], system.equivalence_classes[u1], v)
-                                m2 = _map_indices(system.equivalence_classes[p[1]], system.equivalence_classes[u2], v)
+                                m1 = _map_indices(
+                                    system.equivalence_classes[p[0]], system.equivalence_classes[u1], v, system.no_class
+                                )
+                                m2 = _map_indices(
+                                    system.equivalence_classes[p[1]], system.equivalence_classes[u2], v, system.no_class
+                                )
                                 extended_vars.add(tuple([a + b for a, b in zip(m1, m2)]))
+            else:
+                extended_vars.add(v)
         result.add(tuple(sorted(list(extended_vars))))
     return result
 
