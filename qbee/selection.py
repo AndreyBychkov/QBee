88cc180d5f01cea28218e4a40cbb6e143227b4de
gpogudin <gpogudin@inria.fr>
2023-01-23 13:29:54 +0100

    decoupling generation and scoring

--- qbee/selection.py
+++ qbee/selection.py
@@ -1,34 +1,44 @@
 from sympy.polys.monomials import monomial_deg, monomial_divides
-from typing import Tuple, Callable
+from typing import Tuple, Callable, Iterable, Any
 
-SelectionStrategy = Callable[['PolynomialSystem'], int]
+from .util import *
 
+GenerationStrategy = Callable[['PolynomialSystem'], Iterable[Any]]
+Scoring = Callable[['PolynomialSystem'], int]
+
+# Standard generation strategy with different scoring functions
+
+def default_generation(system):
+    if len(system.nonsquares) == 0:
+        return list()
+    return get_decompositions(system.get_smallest_nonsquare())
+
+# Different scoring functions
 
 def empty_score(system) -> int:
     return 1
 
-
-def default_strategy(system) -> int:
+def default_scoring(system) -> int:
     total_nonsquare = sum([sum(map(abs, m)) for m in system.nonsquares])
     return total_nonsquare + system.dim * len(system.vars)
 
 
-def aeqd_strategy(system) -> int:
+def aeqd_scoring(system) -> int:
     eq_degs = list(map(lambda mlist: max(map(monomial_deg, mlist)), system.rhs.values()))
     aeqds = list(map(_compute_aeqd, system.nonsquares, [eq_degs, ] * len(system.nonsquares)))
     return sum(aeqds)
 
 
-def smd_strategy(system) -> int:
+def smd_scoring(system) -> int:
     mlist = system.nonsquares
     return sum(map(lambda s: _compute_smd(s, mlist), mlist))
 
 
-def _compute_aeqd(sub: Tuple[int], eq_degs):
+def _compute_scoring(sub: Tuple[int], eq_degs):
     mon_degs = map(lambda deg: deg + monomial_deg(sub) - 1, eq_degs)
     quad_discrepancies = filter(lambda x: x > 0, map(lambda d: d - 2, mon_degs))
     return sum(quad_discrepancies)
 
 
-def _compute_smd(sub, mlist: list):
+def _compute_scoring(sub, mlist: list):
     return (monomial_deg(sub) - 1) * len(list(filter(lambda m: monomial_divides(sub, m), mlist)))
