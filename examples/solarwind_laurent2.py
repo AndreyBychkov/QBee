402b8a646f723a971e0e0ed29f3ae786ed2a3262
gpogudin <gpogudin@inria.fr>
2023-01-23 14:48:03 +0100

    first working prototype

--- examples/solarwind_laurent2.py
+++ examples/solarwind_laurent2.py
@@ -3,12 +3,12 @@ from qbee import *
 
 if __name__ == '__main__':
     
-    R, v1, v2, v3 = ring("v1, v2, v3", QQ)
+    R, v1, v2, v3 = ring("v_1, v_2, v_3", QQ)
     #vt = vx * vinv - c * vx
     v1t = (v1 + v2) * v1**(-1) - (v1 + v2)
     v2t = (v1 + v2 + v3) * v2**(-1) + (v1 + v2 + v3)
     v3t = (v2 + v3) * v3**(-1) + (v2 + v3)
 
-    quadr_system = quadratize([v1t, v2t, v3t])
+    quadr_system = quadratize([v1t, v2t, v3t], generation_strategy=generation_semidiscretized)
     if quadr_system:
         print(quadr_system)
--- qbee/selection.py
+++ qbee/selection.py
@@ -13,6 +13,78 @@ def default_generation(system):
         return list()
     return get_decompositions(system.get_smallest_nonsquare())
 
+#####
+
+"""
+Given two mappings from the variables to indices and a list of numbers
+creates a new list in which the values at the indices corresponding to 
+variables in `v_to_ind_from` are placed at positions as prescribed by `v_to_ind_to`
+"""
+def _map_indices(v_to_ind_from, v_to_ind_to, arr):
+    result = [0 for _ in arr]
+    v_to_val = {v: arr[ind] for v, ind in v_to_ind_from.items()}
+    for v, ind in v_to_ind_to.items():
+        result[ind] = v_to_val[v]
+    return result
+
+
+def generation_semidiscretized(system):
+    # recognizing the semidiscretized structure if not cashed already
+    if not hasattr(system, "equivalence_classes"):
+        system.equivalence_classes = dict()
+        system.ind_to_class = dict()
+        system.ind_to_var = dict()
+        for i, s in enumerate(system.gen_symbols):
+            class_ind = int(str(s).split("_")[-1])
+            var_name = "_".join(str(s).split("_")[:-1])
+            if class_ind not in system.equivalence_classes:
+                system.equivalence_classes[class_ind] = dict()
+            system.equivalence_classes[class_ind][var_name] = i
+            system.ind_to_class[i] = class_ind
+            system.ind_to_var[i] = var_name
+
+        # building the graph
+        system.graph = dict()
+        for i, eq in system.rhs.items():
+            c = system.ind_to_class[i]
+            if c not in system.graph:
+                system.graph[c] = set()
+            for m in eq:
+                classes = set([system.ind_to_class[j] for j in range(system.dim) if m[j] != 0])
+                for cl in classes:
+                    if cl != c:
+                        system.graph[c].add(cl)
+
+        print(system.equivalence_classes)
+        print(system.ind_to_class)
+        print(system.ind_to_var)
+        print(system.graph)
+    
+    # producing sets of new variables
+    decomposition = get_decompositions(system.get_smallest_nonsquare())
+    result = set()
+    for d in decomposition:
+        extended_vars = set(d)
+        for v in d:
+            classes = set([system.ind_to_class[j] for j in range(system.dim) if v[j] != 0])
+            assert len(classes) <= 2
+            if len(classes) == 1:
+                c = list(classes)[0]
+                for cl, var_to_ind in system.equivalence_classes.items():
+                    extended_vars.add(tuple(_map_indices(system.equivalence_classes[c], var_to_ind, v)))
+            elif len(classes) == 2:
+                c1, c2 = list(classes)
+                for u1, adj in system.graph.items():
+                    for u2 in adj:
+                        for p in [(c1, c2), (c2, c1)]:
+                            if p[1] in system.graph[p[0]]:
+                                m1 = _map_indices(system.equivalence_classes[p[0]], system.equivalence_classes[u1], v)
+                                m2 = _map_indices(system.equivalence_classes[p[1]], system.equivalence_classes[u2], v)
+                                extended_vars.add(tuple([a + b for a, b in zip(m1, m2)]))
+        result.add(tuple(sorted(list(extended_vars))))
+    return result
+
+
 # Different scoring functions
 
 def empty_score(system) -> int:
