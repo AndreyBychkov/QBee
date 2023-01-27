72d772352c6e44722d9c918714aa0ce9dff1119e
pogudingleb <pogudin.gleb@gmail.com>
2023-01-27 13:27:12 +0100

    solarwind cleanup

--- examples/solarwind.py
+++ examples/solarwind.py
@@ -1,23 +1,15 @@
-import sympy as sp
+from sympy import *
 from qbee import *
 
-
 if __name__ == '__main__':
-    v1, v2, v3, v1inv, v2inv, v3inv = functions("v1, v2, v3, v1inv, v2inv, v3inv")
-    c = parameters("c")
-    #vt = vx * vinv - c * vx
-    v1t = (v1 + v2) * v1inv - c * (v1 + v2)
-    v2t = (v1 + v2 + v3) * v2inv + c * (v1 + v2 + v3)
-    v3t = (v2 + v3) * v3inv + c * (v2 + v3)
-    system = [
-        (v1, v1t),
-        (v2, v2t),
-        (v3, v3t),
-        (v1inv, -v1t * v1inv**2),
-        (v2inv, -v2t * v2inv**2),
-        (v3inv, -v3t * v3inv**2)
-    ]
+    N = 6
+    vs = functions(",".join([f"v_{i}" for i in range(1, N + 1)]))
+    graph = {1: {2, 4}, 2: {3}, 3: {}, 4: {}, 5: {1, 2, 3, 6}, 6: {1, 2, 3}}
+    eqs = []
+    for i in range(1, N + 1):
+        Av = sum([vs[j - 1] for j in graph[i]] + [vs[i - 1]])
+        eqs.append([vs[i - 1], Av * vs[i - 1]**(-1) - Av])
 
-    quadr_system = polynomialize_and_quadratize(system)
+    quadr_system = polynomialize_and_quadratize(eqs, generation_strategy=generation_semidiscretized)
     if quadr_system:
         print(quadr_system)
