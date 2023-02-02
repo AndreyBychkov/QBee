b11b3baeae2d698805bbdb43ae3877e40432ad8f
pogudingleb <pogudin.gleb@gmail.com>
2023-02-03 00:38:27 +0100

    solarwind.py

--- examples/solarwind.py
+++ examples/solarwind.py
@@ -3,20 +3,14 @@ from qbee import *
 
 
 if __name__ == '__main__':
-    v1, v2, v3, v1inv, v2inv, v3inv = functions("v1, v2, v3, v1inv, v2inv, v3inv")
-    c = parameters("c")
-    #vt = vx * vinv - c * vx
-    v1t = (v1 + v2) * v1inv - c * (v1 + v2)
-    v2t = (v1 + v2 + v3) * v2inv + c * (v1 + v2 + v3)
-    v3t = (v2 + v3) * v3inv + c * (v2 + v3)
-    system = [
-        (v1, v1t),
-        (v2, v2t),
-        (v3, v3t),
-        (v1inv, -v1t * v1inv**2),
-        (v2inv, -v2t * v2inv**2),
-        (v3inv, -v3t * v3inv**2)
-    ]
+    N = 4
+    vs = functions(",".join([f"v{i}" for i in range(N)]))
+    us = functions(",".join([f"u{i}" for i in range(N)]))
+    graph = {0: {0, 1, 3}, 1: {1, 2}, 2: {2}, 3: {3}}
+    system = []
+    for i in range(N):
+        system.append((vs[i], sum([us[j] for j in graph[i]]) + sum([vs[j] for j in graph[i]])))
+        system.append((us[i], system[-1][1] / vs[i]))
 
     quadr_system = polynomialize_and_quadratize(system)
     if quadr_system:
