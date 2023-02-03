a1a8c1cb571594f5b9456d0f16587f6c9761cb0d
gpogudin <gpogudin@inria.fr>
2023-02-03 13:53:34 +0100

    simple polynomialization example

---
+++ examples/polynomialization.py
@@ -0,0 +1,9 @@
+import sympy as sp
+from qbee import *
+
+
+if __name__ == '__main__':
+    x = functions("x")
+    poly_system = polynomialize([(x, sp.exp(x) + sp.exp(2 * x))])
+    if poly_system:
+        print(poly_system)
