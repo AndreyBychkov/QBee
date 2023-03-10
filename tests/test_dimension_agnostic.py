14dfe1a31c96fc8a6d40ae2c5f5ad220305040e8
gpogudin <gpogudin@inria.fr>
2023-03-10 16:41:11 +0100

    +testcase

--- tests/test_dimension_agnostic.py
+++ tests/test_dimension_agnostic.py
@@ -1,5 +1,13 @@
 from qbee import *
 
+def test_basic():
+    x, Dx = functions("x Dx")
+    system = [(x, x**2 * Dx)]
+    new_vars = quadratize_dimension_agnostic(system)
+    assert len(new_vars) == 2
+    x, xt = sp.symbols("x x_tilda")
+    assert set(new_vars) == {x**2, x * xt}
+
 def test_solarwind():
     u, v, Du, Dv = functions("u v Du Dv")
     C1 = parameters("C1")
