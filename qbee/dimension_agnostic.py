799545effd6c80ad28634d0280e40b213aa21ec4
pogudingleb <pogudin.gleb@gmail.com>
2023-03-11 21:08:38 +0100

    scalar inputs for differential_agnistic case + tests + docstring

--- qbee/dimension_agnostic.py
+++ qbee/dimension_agnostic.py
@@ -8,7 +8,22 @@ from .quadratization import *
 DA_GRAPH = {0: {0, 1, 3}, 1: {1, 2}, 2: {2}, 3: {3}}
 
 
-def quadratize_dimension_agnostic(system: list[(sp.Symbol, sp.Expr)], non_duplicated_vars: list[sp.Symbol] | None = None):
+def quadratize_dimension_agnostic(system: list[(sp.Symbol, sp.Expr)], 
+                                  non_duplicated_vars: list[sp.Symbol] | None = None,
+                                  input_der_orders=None,
+                                  input_free=False,
+                                  print_intermediate=False):
+    """
+    Find a dimension-agnostic quadratization for a linearly coupled family of ODE system
+
+    :param system: the symbolic representation of the input family, where Dx is the coupling term for the variables x
+    :param non_duplicated_vars: list of variables which are the same in all the blocks of the family (must be inputs)
+    :param input_der_orders: maximal oders for the input variables (if defined, overwrites input_free)
+    :param input_free: if an input-free quadratization is sought
+    :param print_intermediate: if the quadratization of the auxiliary ODE system from Proposition 4.7 should be printed
+    :return: dimension-agnistic quadratization of the input family
+    """
+
     if non_duplicated_vars is None:
         non_duplicated_vars = []
     odesubs = [{} for _ in range(len(DA_GRAPH))]
@@ -34,11 +49,26 @@ def quadratize_dimension_agnostic(system: list[(sp.Symbol, sp.Expr)], non_duplic
             error(f"Non-duplicated variables must not appear on the left-hand side, not true for {lhs}")
         for i in range(len(DA_GRAPH)):
             ode_system.append((lhs.subs(odesubs[i]), rhs.subs(odesubs[i])))
-   
+
+    # transforming the input info
+    ode_input_der_orders = input_der_orders
+    if input_der_orders is not None:
+        ode_input_der_orders = {}
+        for u, order in input_der_orders.items():
+            if u in non_duplicated_vars:
+                ode_input_der_orders[u] = order
+            else:
+                for i in range(len(DA_GRAPH)):
+                    ode_input_der_orders[odesubs[i][u]] = order
+
     # computing ODE quadratization
-    print(ode_system)
-    quadr_system = quadratize(ode_system, generation_strategy=generation_semidiscretized)
-    quadr_system.print()
+    quadr_system = quadratize(ode_system, 
+                              generation_strategy=generation_semidiscretized,
+                              input_free=input_free,
+                              input_der_orders=ode_input_der_orders)
+    if print_intermediate:
+        print("Quadratization of the auxiliary ODE system:")
+        quadr_system.print()
 
     # extracting dimension-agnostic quadratization
     da_new_vars = []
--- tests/test_dimension_agnostic.py
+++ tests/test_dimension_agnostic.py
@@ -8,6 +8,23 @@ def test_basic():
     x, xt = sp.symbols("x x_tilda")
     assert set(new_vars) == {x**2, x * xt}
 
+def test_scalar_input():
+    x, Dx, u = functions("x Dx u")
+    system = [(x, x * u * Dx)]
+    new_vars = quadratize_dimension_agnostic(system, non_duplicated_vars=[u])
+    assert len(new_vars) == 1
+    x, u = sp.symbols("x u")
+    assert new_vars[0] == x * u
+
+def test_scalar_input_free():
+    x, Dx, u = functions("x Dx u")
+    a = parameters("a")
+    system = [(x, x**2 * Dx + a * u * x)]
+    new_vars = quadratize_dimension_agnostic(system, non_duplicated_vars=[u], input_free=True)
+    assert len(new_vars) == 2
+    x, xt = sp.symbols("x x_tilda")
+    assert set(new_vars) == {x**2, x * xt}
+
 def test_solarwind():
     u, v, Du, Dv = functions("u v Du Dv")
     C1 = parameters("C1")
