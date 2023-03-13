3d0aa9b2e949c6121664260ece25f2b22f0d3988
gpogudin <gpogudin@inria.fr>
2023-03-13 14:35:47 +0100

    combustion example: first version

---
+++ examples/example_combistion.py
@@ -0,0 +1,17 @@
+import sympy as sp
+from qbee import *
+
+
+if __name__ == '__main__':
+    x1, x2, x3, x4 = functions("x1, x2, x3, x4")
+    u = functions("u", laurent=False)
+    A, E, R = parameters("A, E, R")
+    x1_diff = -A * sp.exp(-E / (R * u)) * x1**0.2 * x2**1.3
+    system = [
+        (x1, x1_diff),
+        (x2, 2 * x1_diff),
+        (x3, -x1_diff),
+        (x4, -2 * x1_diff)
+    ]
+    quadr_system = polynomialize_and_quadratize(system, input_der_orders={u: 2})
+    quadr_system.print()
