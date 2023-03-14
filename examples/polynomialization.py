fc71611c6b6eb01cc868dfeb057ce6855ab781b1
pogudingleb <pogudin.gleb@gmail.com>
2023-03-14 23:15:54 +0100

    polynomialization example updated

--- examples/polynomialization.py
+++ examples/polynomialization.py
@@ -4,6 +4,6 @@ from qbee import *
 
 if __name__ == '__main__':
     x = functions("x")
-    poly_system = polynomialize([(x, sp.exp(x) + sp.exp(2 * x))])
+    poly_system = polynomialize([(x, sp.exp(-x) + sp.exp(-2 * x))])
     if poly_system:
         poly_system.print()
